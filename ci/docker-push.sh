5975165cb00e985e563ed8c7301e2d4ae0cc9736
Sasha <aleks.romanenko13@gmail.com>
2026-04-20 11:11:58 +0200

    Publish Docker images under selenwright/ namespace
    Rename the published image from aqa-alex/gridlane to selenwright/gridlane
    to match the selenwright/ org on Docker Hub (selenwright/hub already
    lives there). Update the compose example in docs/docker.adoc to reference
    selenwright/hub:latest-release for the sibling selenwright service too.

--- ci/docker-push.sh
+++ ci/docker-push.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-IMAGE="aqa-alex/gridlane"
+IMAGE="selenwright/gridlane"
 
 printf '%s' "$DOCKERHUB_TOKEN" | docker login --username "$DOCKERHUB_USERNAME" --password-stdin
 docker buildx build --pull --push -t "$IMAGE" -t "$IMAGE:$1" --platform linux/amd64,linux/arm64 .
